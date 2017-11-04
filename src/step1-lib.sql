2b37a08ca92cfe7a352f442079e1318babd02c8c
Peter <ppkrauss@gmail.com>
2017-11-04 12:17:44 -0200

    Update step1-lib.sql

--- src/step1-lib.sql
+++ src/step1-lib.sql
@@ -1,18 +1,53 @@
-
+/**
+ * Adds useful general use library (to analyse or normalize CSV cells). 
+ */
 CREATE EXTENSION IF NOT EXISTS fuzzystrmatch; -- for metaphone() and levenshtein()
 CREATE EXTENSION IF NOT EXISTS unaccent; -- for unaccent()
 
-DROP  SCHEMA lib CASCADE;
+DROP SCHEMA lib CASCADE;  -- caution (!) with existing libs
 CREATE SCHEMA lib;
 
 
--- PUBLIC functions
+-- PUBLIC SCHEMA functions
 
+/**
+ * A good workaround solution for an old problem, 
+ * @see https://stackoverflow.com/a/20934099/287948
+ */
+ CREATE FUNCTION ROUND(float,int) RETURNS NUMERIC AS $$
+    SELECT ROUND($1::numeric,$2);
+ $$ language SQL IMMUTABLE;
+
+/**
+ * A general solution for an old problem, 
+ * @see https://stackoverflow.com/a/20934099/287948
+ */
+CREATE or replace FUNCTION ROUND(float, text, int DEFAULT 0)
+RETURNS FLOAT AS $$
+   SELECT CASE WHEN $2='dec'
+               THEN ROUND($1::numeric,$3)::float
+               -- ... WHEN $2='hex' THEN ... WHEN $2='bin' THEN... complete!
+               ELSE 'NaN'::float  -- like an error message
+           END;
+$$ language SQL IMMUTABLE;
+
+/**
+ * Percent avoiding divisions by zero when is ok to do it.
+ */
+CREATE or replace FUNCTION percent(float,float) RETURNS float AS $$
+   SELECT CASE WHEN $1=0.0 OR $1 IS NULL THEN 0.0 ELSE ROUND(100.0*$1/$2)::float END
+$$ language SQL IMMUTABLE;
+CREATE or replace FUNCTION percent(bigint,bigint) RETURNS bigint AS $$
+   SELECT percent($1::float,$2::float)::bigint
+$$ language SQL IMMUTABLE;
+
+
+-- LIB SCHEMA functions 
 -- -- -- -- -- --
 -- Normalize and convert to integer-ranges, for postalCode_ranges.
 -- See section "Preparation" at README.
 
-CREATE or replace FUNCTION csvranges_to_int4ranges(
+CREATE or replace FUNCTION lib.csvranges_to_int4ranges(
   p_range text
 ) RETURNS int4range[] AS $f$
    SELECT ('{'||
@@ -21,20 +56,14 @@ CREATE or replace FUNCTION csvranges_to_int4ranges(
 $f$ LANGUAGE SQL IMMUTABLE;
 
 
-CREATE or replace FUNCTION int4ranges_to_csvranges(
+CREATE or replace FUNCTION lib.int4ranges_to_csvranges(
   p_range int4range[]
 ) RETURNS text AS $f$
    SELECT translate($1::text,',{}"',' ');
 $f$ LANGUAGE SQL IMMUTABLE;
 
 
--- -- -- -- -- --
--- LIB and extras 
--- See https://github.com/datasets-br/diariosOficiais/blob/master/src/step1_strut.sql
-
-
-
-CREATE FUNCTION lib.normalizeterm(
+CREATE or replace FUNCTION lib.normalizeterm(
 	--
 	-- Converts string into standard sequence of lower-case words.
 	--
@@ -55,77 +84,3 @@ CREATE FUNCTION lib.normalizeterm(
   );
 $f$ LANGUAGE SQL IMMUTABLE;
 
-
-
-CREATE FUNCTION lib.normalizeterm2(
-	text,
-	boolean DEFAULT true
-) RETURNS text AS $f$
-   SELECT (  lib.normalizeterm(
-          CASE WHEN $2 THEN substring($1 from '^[^\(\)\/;]+' ) ELSE $1 END,
-	  ' ',
-	  320,
-          ' / '
-   ));
-$f$ LANGUAGE SQL IMMUTABLE;
-
-
-CREATE or replace FUNCTION lib.name2lex(
-  p_name text,
-  p_normalize boolean DEFAULT true,
-  p_cut boolean DEFAULT true
-) RETURNS text AS $f$
-   SELECT unaccent(trim(replace(
-	   regexp_replace(
-	     CASE WHEN p_normalize THEN lib.normalizeterm2($1,p_cut) ELSE $1 END,
-	     E' d[aeo] | d[oa]s | com | para |^d[aeo] | / .+| [aeo]s | [aeo] |[\-\' ]',
-	     '.',
-	     'g'
-	   ),
-	   '..',
-           '.'
-       ),'.'))
-$f$ LANGUAGE SQL IMMUTABLE;
-
-
-/**
- * Base36 integer conversion.
- * @see https://gist.github.com/btbytes/7159902
- */
-CREATE or replace FUNCTION base36_encode(IN digits bigint, IN min_width int = 0) RETURNS varchar AS $$
-DECLARE
-    chars char[]; 
-    ret varchar; 
-    val bigint; 
-BEGIN
-    chars := array['0','1','2','3','4','5','6','7','8','9',
-      'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'  
-    ];
-    val := digits; 
-    ret := ''; 
-    IF val < 0 THEN 
-        val := val * -1; 
-    END IF; 
-    WHILE val != 0 LOOP 
-        ret := chars[(val % 36)+1] || ret; 
-        val := val / 36; 
-    END LOOP;
-
-    IF min_width > 0 AND char_length(ret) < min_width THEN 
-        ret := lpad(ret, min_width, '0'); 
-    END IF;
-
-    RETURN ret;
-END;
-$$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE or replace FUNCTION lib.br_city_id_gap(text) RETURNS int AS $f$
--- Gap maximo de 10 conforme convenção códigos IBGE. Avaliação de balanço inspirada pela raiz quadrada:
--- SELECT json_build_object(i,s) from (select substr(unaccent(name),1,1) as i, count(*) as n, sqrt(count(*))::int as s 
---    from tmpcsv_cities_wiki group by 1 order by 2 desc) t;
-
-	SELECT CASE WHEN jgap->ini IS NULL THEN 3 ELSE (jgap->>ini)::int END 
-	FROM  
-	(SELECT upper(substr(unaccent($1),1,1)) as ini) t1,
-	(SELECT '{"S":10,"C":10,"P":9,"A":8,"M":8,"I":8,"B":8,"T":6,"J":6,"R":6,"N":6,"G":6,"L":6,"F":4,"V":4,"D":4,"E":4,"O":4,"U":4}'::JSON as jgap) t2;
-$f$ LANGUAGE SQL IMMUTABLE;
